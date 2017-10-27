acc688993b77a47a074618d78812a75835167c01
Davor Barishic-Jaman <cro.davor@outlook.com>
2017-10-27 23:28:01 +0200

    resena lab02/zad02 (kompanija

--- lab02/kompanija.c
+++ lab02/kompanija.c
@@ -22,54 +22,89 @@ struct node {
 	struct node *next;
 };
 
-struct node *new_list(void)
-{
-	struct node *n = malloc(sizeof(struct node));
-	n->prev = n;
-	n->next = NULL;
-	return n;
-}
+struct node *head;
 
-void append(struct node *current, int ID, int plata)
+struct node *newNode(int ID, int plata)
 {
 	struct node *new = malloc(sizeof(struct node));
 	new->ID = ID;
 	new->plata = plata;
+	new->next = NULL;
+	new->prev = NULL;
 
-	while (current->next != NULL) {
+	return new;
+}
+void append(struct node *new)
+{
+	struct node *current = head;
+	assert(new != NULL);
+
+	if (head == NULL) {
+		head = new;
+		return;
+	}
+
+	while (current != NULL) {
+		if (current->ID < new->ID) {
+			if (current == head) {
+				head = new;
+				new->next = current;
+				current->prev = new;
+				return;
+			} else {
+				new->next = current;
+				new->prev = current->prev;
+				current->prev->next = new;
+				current->prev = new;
+				return;
+			}
+		}
+
+		if (current->next == NULL) break;
 		current = current->next;
 	}
 
-	new->next = NULL;
 	new->prev = current;
 	current->next = new;
+	new->next = NULL;
 }
 
 void delete (struct node *deleted)
 {
-	deleted->prev->next = deleted->next;
-	deleted->next->prev = deleted->prev;
+	if (head == NULL)
+		return;
+
+	if (deleted == head) {
+		head = deleted->next;
+		free(deleted);
+		return;
+	}
+
+	if (deleted->prev != NULL)
+		deleted->prev->next = deleted->next;
+	if (deleted->next != NULL)
+		deleted->next->prev = deleted->prev;
+
 	free(deleted);
 }
 
-void deleteLess(struct node *list, int plata)
+void removePoor(int plata) // :'(
 {
-	struct node *p = list;
+	struct node *p = head;
 
-	while (p->next != NULL) {
-		if (p->plata < plata) {
-			delete (p);
-		}
-
+	while (p != NULL) {
+		if (p->plata < plata) delete (p);
 		p = p->next;
 	}
 }
 
-void printList(struct node *list)
+void printList()
 {
-	while (list != NULL) {
-		printf("%d %d \n", list->ID, list->plata);
-		list = list->next;
+	struct node *p = head;
+
+	while (p != NULL) {
+		printf("%d %d\n", p->ID, p->plata);
+		p = p->next;
 	}
 }
 
@@ -78,17 +113,18 @@ int main(void)
 	int n, id, plata, threshold;
 	scanf("%d", &n);
 
-	struct node *list = new_list();
+	head = NULL;
 
 	for (int i = 0; i < n; i++) {
 		scanf("%d", &id);
 		scanf("%d", &plata);
 
-		append(list, id, plata);
-		// insert_node(id, plata);
+		struct node *new = newNode(id, plata);
+
+		append(new);
 	}
 
 	scanf("%d", &threshold);
-	deleteLess(list, threshold);
-	printList(list);
+	removePoor(threshold);
+	head != NULL ? printList() : printf("nema\n");
 }
